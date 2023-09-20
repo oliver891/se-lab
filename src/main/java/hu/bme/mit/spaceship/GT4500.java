b2d1bbd20c3b3a922d264e905a050cdbff353f08
oliver891 <olifarkas891@gmail.com>
2023-09-20 17:07:57 +0200

    Implemented firetorpedoall function

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,8 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        firingSuccess = primaryTorpedoStore.fire(1) || secondaryTorpedoStore.fire(1);
+        wasPrimaryFiredLast = false;
 
         break;
     }
